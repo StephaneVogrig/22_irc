d59900e6c8f84bdfaf0ac9d823e3ab8d55fde9dd
Stephane Vogrig <svogrig@3D3.42angouleme.fr>
2025-03-11 18:01:39 +0100

    utilisation de throw pour la gestion d'erreur

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 13:34:34 by svogrig           #+#    #+#             */
-/*   Updated: 2025/03/11 17:45:02 by svogrig          ###   ########.fr       */
+/*   Updated: 2025/03/11 18:00:25 by svogrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,46 +15,41 @@
 #include <climits>
 #include <iostream>
 
-#define FAILURE 0
-#define SUCCESS 1
-
 int convert_port(const char * str)
 {
 	char *		endptr = NULL;
 	long int	nbr = strtol(str, &endptr, 10);
 	if (*endptr != '\0')
-	{
-		std::cerr << "bad argument!" << std::endl;
-		return (INT_MAX);
-	}
+		throw (std::runtime_error("bad argument!"));
 	if (nbr < 0 || nbr > 65535)
-	{
-		std::cerr << "port must be between 0 65535" << std::endl;
-		return (INT_MAX);
-	}
+		throw (std::runtime_error("port must be between 0 65535"));
 	return (nbr);
 }
 
-int	check_arg(int argc)
+void	check_arg(int argc)
 {
 	if (argc != 3)
 	{
 		std::cerr << "wrong number argument !" << std::endl;
 		std::cerr << "usage :" << std::endl;
 		std::cerr << "./ircserv <port> <password>" << std::endl;
-		return (FAILURE);
+		throw (std::runtime_error(""));
 	}
-	return (SUCCESS);
 }
 
 int main(int argc, char **argv)
 {
-	if (!check_arg(argc))
+	try
+	{
+		check_arg(argc);
+		int port = convert_port(argv[1]);
+		(void)port;
+		std::cout << "hello ircserv" << std::endl;
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
 		return (EXIT_FAILURE);
-	argv++;
-	int port = convert_port(*argv);
-	if (port == INT_MAX)
-		return (EXIT_FAILURE);
-	std::cout << "hello ircserv" << std::endl;
+	}
 	return (EXIT_SUCCESS);
 }
