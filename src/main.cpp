1482353c014936303873a9ef348ffcaa85406aa0
Stephane Vogrig <svogrig@3D3.42angouleme.fr>
2025-03-11 18:09:09 +0100

    ajout de la couleur pour les messages

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 13:34:34 by svogrig           #+#    #+#             */
-/*   Updated: 2025/03/11 18:00:25 by svogrig          ###   ########.fr       */
+/*   Updated: 2025/03/11 18:07:26 by svogrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,24 @@
 #include <climits>
 #include <iostream>
 
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define PURPLE "\033[35m"
+#define CYAN "\033[36m"
+#define GREY "\033[37m"
+#define RESET "\033[0m"
+#define GREEN_BLINK	"\033[5;32m"
+
 int convert_port(const char * str)
 {
 	char *		endptr = NULL;
 	long int	nbr = strtol(str, &endptr, 10);
 	if (*endptr != '\0')
-		throw (std::runtime_error("bad argument!"));
+		throw (std::runtime_error(RED "bad argument!" RESET));
 	if (nbr < 0 || nbr > 65535)
-		throw (std::runtime_error("port must be between 0 65535"));
+		throw (std::runtime_error(RED "port must be between 0 65535" RESET));
 	return (nbr);
 }
 
@@ -30,10 +40,9 @@ void	check_arg(int argc)
 {
 	if (argc != 3)
 	{
-		std::cerr << "wrong number argument !" << std::endl;
-		std::cerr << "usage :" << std::endl;
-		std::cerr << "./ircserv <port> <password>" << std::endl;
-		throw (std::runtime_error(""));
+		std::cerr << RED "wrong number argument !" RESET << std::endl;
+		std::cerr << YELLOW "usage :" RESET << std::endl;
+		throw (std::runtime_error(YELLOW "./ircserv <port> <password>" RESET));
 	}
 }
 
@@ -44,7 +53,7 @@ int main(int argc, char **argv)
 		check_arg(argc);
 		int port = convert_port(argv[1]);
 		(void)port;
-		std::cout << "hello ircserv" << std::endl;
+		std::cout << GREEN_BLINK "hello ircserv" RESET << std::endl;
 	}
 	catch (const std::exception& e)
 	{
