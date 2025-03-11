2f26a1c90ebe10440fd941ebad7aa92d3bfec7f9
Gael Cannaud <gcannaud@3D1.42angouleme.fr>
2025-03-11 17:11:13 +0100

    fonction check_arg dans le main

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,43 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
+/*   By: gcannaud <gcannaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 13:34:34 by svogrig           #+#    #+#             */
-/*   Updated: 2025/03/11 13:36:59 by svogrig          ###   ########.fr       */
+/*   Updated: 2025/03/11 17:10:31 by gcannaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <iostream>
 
-int main(void)
+#define FAILURE 0
+#define SUCCESS 1
+
+int	check_arg(int argc, char **argv)
 {
+	(void) argv;
+	if (argc != 3)
+	{
+		std::cerr << "wrong number argument !" << std::endl;
+		std::cerr << "usage :" << std::endl;
+		std::cerr << "./ircserv <port> <password>" << std::endl;
+		return (FAILURE);
+	}
+	
+	int	port = atoi(argv[1]);
+	if (port < 0 || port > 65535)
+	{
+		std::cerr << "port must be between 0 65535" << std::endl;
+		return (FAILURE);
+	}
+	return (SUCCESS);
+}
+
+int main(int argc, char **argv)
+{
+	if (!check_arg(argc, argv))
+		return (EXIT_FAILURE);
 	std::cout << "hello ircserv" << std::endl;
 	return (EXIT_SUCCESS);
 }
