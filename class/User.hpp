c654ccc881b6b61fdc1d8345a1e0fadf77500269
Stephane Vogrig <svogrig@3B3.42angouleme.fr>
2025-03-17 18:03:05 +0100

    add class User

---
+++ class/User.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   User.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/17 17:26:21 by svogrig           #+#    #+#             */
+/*   Updated: 2025/03/17 18:01:55 by svogrig          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef USER_HPP
+# define USER_HPP
+
+# include <string>
+
+class User
+{
+	public:
+
+		User(const std::string & userName, const std::string & nickName);
+		User(const User & toCopy);
+		~User();
+
+		User & operator = (const User & toAssign);
+
+	private:
+
+		std::string _userName;
+		std::string _nickName;
+
+};
+
+#endif
