c49660997001d757923899c477931cd18c350cc9
Gael Cannaud <gcannaud@3E2.42angouleme.fr>
2025-04-02 17:52:35 +0200

    quit modif error

--- class/command/Quit.cpp
+++ class/command/Quit.cpp
@@ -6,7 +6,7 @@
 /*   By: gcannaud <gcannaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 16:41:06 by gcannaud          #+#    #+#             */
-/*   Updated: 2025/04/02 17:38:45 by gcannaud         ###   ########.fr       */
+/*   Updated: 2025/04/02 17:50:47 by gcannaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,24 +23,23 @@ Quit::~Quit()
 
 void Quit::exec(Client & client, const Params & params, Server & Server)
 {
-    (void) Server;
-    (void) params;
+	(void) Server;
 
-    std::string msg;
-    try
-    {
-        if (params.get_nbr() > 1)
-            ERR_NEEDMOREPARAMS(client, "QUIT");
-        if (params.get_nbr() == 1)
-            msg = params.get_param(0);
-        if (msg.empty())
-            ERR_NOTEXTTOSEND(client);
-    }
-    catch(const Protocole_error& e)
-    {
-    }
+	std::string msg;
+	try
+	{
+		if (params.get_nbr() > 1)
+			ERR_NEEDMOREPARAMS(client, "QUIT");
+		if (params.get_nbr() == 1)
+			msg = params.get_param(0);
+		if (params.get_nbr() == 1 && msg.empty())
+			ERR_NOTEXTTOSEND(client);
+	}
+	catch(const Protocole_error& e)
+	{
+	}
 
-    client.quit_serv_channels(msg);
+	client.quit_serv_channels(msg);
 	client.send_msg(":" + client.get_nickname() + " QUIT :" + msg);
-    client.set_kicked(true);
+	client.set_kicked(true);
 }
