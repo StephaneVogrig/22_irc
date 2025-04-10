936f75a80456d6fb3171e8fe4a6ad7c243c3176f
Stephane Vogrig <svogrig@3C6.42angouleme.fr>
2025-04-10 13:16:52 +0200

    WHO: fix par rapport a server de reference

--- class/command/Who.cpp
+++ class/command/Who.cpp
@@ -6,7 +6,7 @@
 /*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 17:02:11 by gcannaud          #+#    #+#             */
-/*   Updated: 2025/04/08 13:38:41 by svogrig          ###   ########.fr       */
+/*   Updated: 2025/04/10 13:13:59 by svogrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,10 @@
 #include "Server.hpp"
 
 Who::Who() : Command("WHO")
-{
-}
+{}
 
 Who::~Who()
-{
-}
+{}
 
 void Who::exec(Client & client, const Params & params, Server & server)
 {
@@ -30,18 +28,15 @@ void Who::exec(Client & client, const Params & params, Server & server)
 		exec_on_channel(client, params, server);
 	else
 		exec_on_user(client, params, server);
+	RPL_315_ENDOFWHO(client);
 }
 
 void Who::exec_on_channel(Client & client, const Params & params, Server & server)
 {
 	const std::string & channel_name = params.get_first();
-	if (!server.channel_exist(channel_name))
-		RPL_318_ENDOFWHOIS(client, params.get_first());
 	Channel * channel = server.get_channel(params.get_first());
-	if (channel == NULL)
-		ERR_403_NOSUCHCHANNEL(client, params.get_first());
-	channel->send_who(client, server);
-	RPL_315_ENDOFWHO(client);
+	if (channel != NULL)
+		channel->send_who(client, server);
 }
 
 void Who::exec_on_user(Client & client, const Params & params, Server & server)
@@ -49,19 +44,17 @@ void Who::exec_on_user(Client & client, const Params & params, Server & server)
 	Client * target = server.get_client_by_name(params.get_first());
 	if (target != NULL)
 	{
+		std::string flags("H");
+		std::string channel_name;
 		Channel *channel = target->get_last_channel_subscripted();
 		if (!channel)
+			channel_name = "*";
+		else
 		{
-			RPL_352_WHOREPLY(client, *target, server, "*", "H");
-			return ;
-		}
-		if (channel->is_join(*target))
-		{
-			std::string flags("H");
-			if (channel->get_client_status(*target).find("o") != std::string::npos)
+			channel_name = channel->get_name();
+			if (channel->is_operator(client))
 				flags += "*";
-			RPL_352_WHOREPLY(client, *target, server, channel->get_name(), flags);
 		}
+		RPL_352_WHOREPLY(client, *target, server, channel_name, flags);
 	}
-	RPL_315_ENDOFWHO(client);
 }
