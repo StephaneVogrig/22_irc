ebd9a8cb79ec7a140ea840f1ec174e6245642f83
Gael Cannaud <gcannaud@2D6.42angouleme.fr>
2025-04-11 15:39:59 +0200

    fix ping msg

--- class/command/Ping.cpp
+++ class/command/Ping.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Ping.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
+/*   By: gcannaud <gcannaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 16:41:06 by gcannaud          #+#    #+#             */
-/*   Updated: 2025/04/10 16:45:01 by svogrig          ###   ########.fr       */
+/*   Updated: 2025/04/11 15:38:53 by gcannaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,5 @@ void Ping::exec(Client & client, const Params & params, Server & server)
     if (params.get_nbr() < 1)
         ERR_461_NEEDMOREPARAMS(client, "PING", server);
 
-    client.send_msg(":" + server.get_name() + " PONG :" + params.get_first());
+    client.send_msg(":" + server.get_name() + " PONG " + server.get_name() + " :" + params.get_first());
 }
